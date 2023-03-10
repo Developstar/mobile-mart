90275c05852ecb48c59bc08875e129949f228274
Developstar <deyeye99@gmail.com>
2023-03-10 21:00:26 +0100

    created a const added all routes

--- src/main.js
+++ src/main.js
@@ -1,10 +1,29 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import {createRouter, createWebHistory} from "vue-router"
+import HomePage from "./components/views/HomePage.vue"
+import LogIn from "./components/views/LogIn.vue"
+import AllProducts from "./components/views/AllProducts.vue"
 
 const router = createRouter({
     history: createWebHistory(),
-    routes: []
+    routes: [{
+        path: "/",
+        name: "HomePage",
+        component: HomePage
+    },
+    {
+        path: "/LogIn",
+        name: "LogIn",
+        component: LogIn
+    },
+    {
+        path: "/AllProducts",
+        name: "AllProducts",
+        component: AllProducts
+    }
+]
+
 })
 
 createApp(App).use(router).mount('#app')
