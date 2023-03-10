888fae42186def23fc61253353baacdeff4e0684
Developstar <deyeye99@gmail.com>
2023-03-10 20:58:33 +0100

    created homepage route

---
+++ src/components/views/HomePage.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h1>Home Page</h1>
+        <p>This is Home Page</p>
+        <router-link to="/LogIn">LogIn</router-link>
+        <router-link to="/AllProducts">Products</router-link>
+
+    </div>
+</template>
+
+
