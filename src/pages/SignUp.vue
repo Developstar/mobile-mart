37163cfa5aeff319487b6a1388545d6bd094778a
Developstar <deyeye99@gmail.com>
2023-03-11 07:06:20 +0100

    Moved component from views to Pages

---
+++ src/pages/SignUp.vue
@@ -0,0 +1,7 @@
+<template>
+    <h1>SignUp</h1>
+    <p>This is SignUp Page</p>
+    <router-link to="/">Home</router-link>
+    <router-link to="/AllProducts">AllProducts</router-link>
+    <router-link to="/LogIn">LogIn</router-link>
+</template>
