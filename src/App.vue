3b49c1bfc72a14844bb2833a88e496a1fac330d4
Developstar <deyeye99@gmail.com>
2023-03-11 14:21:35 +0100

    added NavBar and styled page body

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,40 @@
 <template>
+  <section class="app">
+    <NavBar/>
   <router-view></router-view>
+  </section>
+  
 </template>
 
 <script>
-
-
+import NavBar from "./components/NavBar.vue"
 export default {
   name: 'App',
   components: {
-    
+    NavBar,
   }
 }
 </script>
 
 <style>
+*{
+  margin: 0 auto;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body{
+  background: #e3e7eb;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  /* font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 60px; */
+  margin: 0 auto;
+  box-sizing: border-box;
+  
 }
 </style>
